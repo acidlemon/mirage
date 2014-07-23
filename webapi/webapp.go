31a680736a5c339f609c1ea309f6c21bcf0f9d47
Masatoshi Kawazoe (acidlemon) <acidlemon@beatsync.net>
2014-07-23 11:02:58 +0900

    work in progress

--- webapi/webapp.go
+++ webapi/webapp.go
@@ -13,9 +13,10 @@ type WebApi struct {
 }
 
 
-func NewWebApi() (WebApi) {
+func NewWebApi(host string) (WebApi) {
 	app := WebApi{}
 	app.Init()
+	app.Host = host
 
 	app.AddRoute("/", app.List, &rocket.View{})
 
@@ -25,11 +26,16 @@ func NewWebApi() (WebApi) {
 }
 
 func (app *WebApi) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	
+	app.Handler(w, req)
 }
 
 func (app *WebApi) List(c rocket.CtxData) {
-	
+	c.Res().StatusCode = http.StatusOK
+	value := rocket.RenderVars {
+		"test" : "powawa",
+	}
+
+	c.Render("webapi/list.html", value)
 }
 
 
