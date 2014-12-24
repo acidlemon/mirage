e7a76a52394dfe6d84a99df3b931a90a24e09e29
Masatoshi Kawazoe (acidlemon) <acidlemon@beatsync.net>
2014-12-24 11:29:38 +0900

    diet docker image

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -rm -v /mirage -v /var/run/docker.sock:/var/run/docker.sock  -t mirage:latest
+docker run --rm -v /mirage -v /var/run/docker.sock:/var/run/docker.sock  -t mirage:latest
