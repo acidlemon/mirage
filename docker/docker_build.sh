668e79db49f769f9d987834e8a9f7866d902c200
Ichinose Shogo <shogo82148@gmail.com>
2015-01-25 10:33:01 +0900

    more diet docker image

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+# download mirage
+LATEST_VERSION=$(curl https://api.github.com/repos/acidlemon/mirage/releases | python -c 'import sys; import json; print(json.loads(sys.stdin.read()))[0]["tag_name"]');
+curl -L -o mirage.zip https://github.com/acidlemon/mirage/releases/download/${LATEST_VERSION}/mirage-${LATEST_VERSION}-linux-amd64.zip
+unzip mirage.zip
+
+# build
 docker build --rm -t mirage .
 
+# clean up
+rm -rf mirage mirage.zip
